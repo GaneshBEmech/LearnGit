8b723f653a9aff3e65b5f588fe3bf412b3b0155b
GaneshBEmech <gbhosale@google.com>
2026-03-19 15:11:12 +0000

    added GCE code

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
-hello
+resource "google_storage_bucket" "GCS1" {
+   name = "my-project-3-358101-bucket-tf-new"
+   location = "us-central1"
+   storage_class = "NEARLINE"
+   lifecycle_rule {
+     action {
+       type = "SetStorageClass"
+       storage_class = "COLDLINE"
+     }
+     condition {
+       age = 10
+     }
+   }
+}
