7a8a3bf412f7c066c6a27d15d47d8be5d19d2c22
GaneshBEmech <gbhosale@google.com>
2026-03-25 07:45:51 +0000

    updated bucket name

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
 
 
 resource "google_storage_bucket" "GCS1" {
-   name = "my-project-3-358101-bucket-tf-new-update"
+   name = "my-project-3-358101-bucket-tf-new-update-1"
    location = "us-central1"
    storage_class = "NEARLINE"
    lifecycle_rule {
